f613a046a36cb35896c84a1c8033344bc882782d
Saber Nouira <contact@wirk.fr>
2025-01-01 14:08:17 +0100

    renamed sketchybar package & removed unused pkgs

--- sketchybar-fixed-app-icon.rb
+++ sketchybar-fixed-app-icon.rb
@@ -2,8 +2,8 @@
 
 # frozen_string_literal: true
 
-# sketchybar.rb
-class Sketchybar < Formula
+# sketchybar-fixed-app-icon.rb
+class Sketchybar_Fixed_App_Icon < Formula
   env :std
   desc "Forked version of SketchyBar with modified base app icon resolution. Originaly forked from FelixKratz/SketchyBar"
   homepage "https://github.com/iWirk/SketchyBar"
